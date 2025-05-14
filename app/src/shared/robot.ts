ece0c275a51d54ca0447e9c427a6128fc24dc078
Sma1lboy <541898146chen@gmail.com>
2025-05-13 21:51:55 -0500

    refactor(robot): improve robotjs loading for development and production environments
    - Updated robotjs loading logic to differentiate between development and production environments.
    - Simplified the require statement for development while maintaining the existing path for production packaging.

--- app/src/shared/robot.ts
+++ app/src/shared/robot.ts
@@ -1,15 +1,25 @@
 import path from "path";
 
-// use to pack the app
-const bin = path.join(
-  process.resourcesPath, // …/FoxyChat.app/Contents/Resources
-  "app.asar.unpacked",
-  "node_modules",
-  "@hurdlegroup",
-  "robotjs",
-  "build",
-  "Release",
-  "robotjs.node",
-);
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-export default require(bin);
+let robotjs;
+
+if (process.env.NODE_ENV === "development") {
+  // For development
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  robotjs = require("@hurdlegroup/robotjs");
+} else {
+  // use to pack the app
+  const bin = path.join(
+    process.resourcesPath,
+    "app.asar.unpacked",
+    "node_modules",
+    "@hurdlegroup",
+    "robotjs",
+    "build",
+    "Release",
+    "robotjs.node",
+  );
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  robotjs = require(bin);
+}
+
+export default robotjs;
